cca83c3085b5cc4e7617346a798621fecd8437eb
User <gabrielkleber006@gmail.com>
2021-07-10 11:08:40 -0300

    requisito 7;  continuando

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const txtInput = document.querySelector("#texto-tarefa")
 const olTarefas = document.querySelector("#lista-tarefas")
 const btnCriarTarefa = document.querySelector("#criar-tarefa")
-btnCriarTarefa.innerText ="adicionar tarefa"
+btnCriarTarefa.innerText = "adicionar tarefa"
 
-btnCriarTarefa.addEventListener("click", addList)
-function addList(){
-const liList = document.createElement("li")
-let txtValue = txtInput.value;
-liList.innerText = txtValue
-olTarefas.appendChild(liList);
-txtValue = txtInput.value =""
-}
+    btnCriarTarefa.addEventListener("click", addList)
+    function addList() {
+        const liList = document.createElement("li")
+        let txtValue = txtInput.value;
+        liList.innerText = txtValue
+        olTarefas.appendChild(liList);
+        txtValue = txtInput.value = ""
+    }
+
+olTarefas.addEventListener("click", function(event){
+    
+    for(i = 0; i < olTarefas.children.length; i += 1) {
+        olTarefas.children[i].style.backgroundColor ="white" // percorre todas os elementos da lista  e deixa o fundo branco
+    }
+    event.target.style.backgroundColor ="rgb(128,128,128)"
+})
+
